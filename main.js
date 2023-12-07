e29367812dc622d6fb1bb0dadefb11c03d350b8e
Scara-08 <149238694+Scara-08@users.noreply.github.com>
2023-12-07 12:14:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const fruits = ["kiwi", "apple", "orange", "grape", "kiwi", "apple", "mango"];
+
+
+
+
+const uniqueFruits = [...new Set(fruits)];
+
+console.log(uniqueFruits);
+
+
+
+["kiwi", "apple", "orange", "grape", "mango"]
+
+
+
+
+
